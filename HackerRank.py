c42f8a0ad285ed563ebbb2bb13c8605db608704d
Prajwal <prajwalsitoula8848@gmail.com>
2025-06-09 12:46:56 +0545

    day 4

--- HackerRank.py
+++ HackerRank.py
@@ -48,32 +48,60 @@
 
     
 
-if __name__ == '__main__':
-    N = int(input())
-    arr = []
+# if __name__ == '__main__':
+#     N = int(input())
+#     arr = []
     
-    for _ in range(N):
-        command = input().strip().split()
+#     for _ in range(N):
+#         command = input().strip().split()
         
-        if command[0] == "insert":
-            arr.insert(int(command[1]), int(command[2]))
-        elif command[0] == "print":
-            print(arr)
-        elif command[0] == "remove":
-            arr.remove(int(command[1]))
-        elif command[0] == "append":
-            arr.append(int(command[1]))
-        elif command[0] == "sort":
-            arr.sort()
-        elif command[0] == "pop":
-            arr.pop()
-        elif command[0] == "reverse":
-            arr.reverse()
+#         if command[0] == "insert":
+#             arr.insert(int(command[1]), int(command[2]))
+#         elif command[0] == "print":
+#             print(arr)
+#         elif command[0] == "remove":
+#             arr.remove(int(command[1]))
+#         elif command[0] == "append":
+#             arr.append(int(command[1]))
+#         elif command[0] == "sort":
+#             arr.sort()
+#         elif command[0] == "pop":
+#             arr.pop()
+#         elif command[0] == "reverse":
+#             arr.reverse()
 
 
 # n = input("Enter anything ").strip().split()
 # print(n)
 
+# def swap_case(s):
+#     result = ""
+#     for char in s:
+#         if char.isupper():
+#             result += char.lower()
+#         elif char.islower():
+#             result += char.upper()
+#         else:
+#             result += char
+#     return result
+
+# if __name__ == '__main__':
+#     s = input()
+#     result = swap_case(s)
+#     print(result)
 
 
 
+
+
+
+# def split_and_join(line):
+#     result = ""
+#     for char in line:
+#         result = result + "-".join(char.split())
+#     return result
+
+# if __name__ == '__main__':
+#     line = input()
+#     result = split_and_join(line)
+#     print(result)
